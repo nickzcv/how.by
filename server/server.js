f99833665257c3d9622d7f732d3ba183ecf488c4
Mikalai Zaitsau <ZaitsauM@iba>
2016-09-16 11:51:24 +0300

    api/customer changes

--- server/server.js
+++ server/server.js
@@ -29,7 +29,39 @@ router.get('/', function(req, res) {
 	res.json({ message: 'How are you? welcome to our api!' });
 });
 
+// middleware to use for all requests
+router.use(function(req, res, next) {
+	// do logging
+	console.log('Something is happening.');
+	next(); // make sure we go to the next routes and don't stop here
+});
+
 // more routes for our API will happen here
+
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/customer')
+
+// create a Customer (accessed at POST http://localhost:8080/api/customer)
+	.post(function(req, res) {
+
+		var customer = new Customer();
+
+		customer.login = req.body.login;
+		customer.password = req.body.password;
+		customer.name = req.body.name;
+		customer.phone = req.body.phone;
+		customer.registrationDate = new Date();
+
+		// save the bear and check for errors
+		customer.save(function(err) {
+			if (err)
+				res.send(err);
+
+			res.json({ message: 'Customer created!' });
+		});
+
+	});
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
@@ -38,4 +70,4 @@ app.use('/api', router);
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('How.by run on port: ' + port);
+console.log('How.by fire on port: ' + port);
