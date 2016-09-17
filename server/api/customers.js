6d81465e2ed12279ad1795b6a944595a275b8347
Nick Z <nickzcv@gmail.com>
2016-09-17 07:54:06 +0300

    add routers - api/customers

---
+++ server/api/customers.js
@@ -0,0 +1,111 @@
+var express = require('express');
+var router = express.Router();              // get an instance of the express Router
+//var bodyParser = require('body-parser');
+
+//Define mongoose Schema
+var Customer = require('../../app/models/customer');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+	res.json({ message: 'How are you? welcome to our api!' });
+});
+
+// on routes that end in /customers
+// ----------------------------------------------------
+router.route('/customers.js')
+
+// create a Customer (accessed at POST http://localhost:8080/api/customer)
+	.post(function(req, res) {
+
+		var customer = new Customer();
+
+		customer.name = req.body.name;
+		customer.username = req.body.username;
+		customer.password = req.body.password;
+		customer.status = 'NEW';
+		customer.location = req.body.location;
+		customer.meta.age = req.body.age;
+		customer.meta.website = req.body.website;
+		customer.meta.email = req.body.email;
+		customer.meta.phone = req.body.phone;
+		customer.created_at = new Date();
+		customer.updated_at = new Date();
+
+		// save the customer and check for errors
+		customer.save(function(err) {
+			if (err)
+				res.send(err);
+
+			res.json({ message: 'Customer created!' });
+		});
+
+	})
+
+	// get all the customers (accessed at GET http://localhost:8080/api/customer)
+	.get(function(req, res) {
+		Customer.find(function(err, customer) {
+			if (err)
+				res.send(err);
+
+			res.json(customer);
+		});
+	});
+
+// on routes that end in /customers/:customer_id
+// ----------------------------------------------------
+router.route('/customers/:customer_id')
+
+// get the customer with that id (accessed at GET http://localhost:8080/api/customers/:customer_id)
+	.get(function(req, res) {
+		Customer.findById(req.params.customer_id, function(err, customer) {
+			if (err)
+				res.send(err);
+
+			res.json(customer);
+		});
+	})
+	// update
+	.put(function(req, res) {
+		Customer.findById(req.params.customer_id, function(err, customer) {
+
+			if (err)
+				res.send(err);
+
+			customer.name = req.body.name;
+			customer.location = req.body.location;
+			customer.meta.age = req.body.age;
+			customer.meta.website = req.body.website;
+			customer.meta.email = req.body.email;
+			customer.meta.phone = req.body.phone;
+			customer.updated_at = new Date();
+
+			customer.save(function(err) {
+				if (err)
+					res.send(err);
+
+				res.json({ message: 'Customer updated!' });
+			});
+
+		});
+	})
+
+	// delete
+	.delete(function(req, res) {
+		Customer.remove({
+			_id: req.params.customer_id
+		}, function(err, customer) {
+			if (err)
+				res.send(err);
+
+			res.json({ message: 'Successfully deleted' });
+		});
+	});
+
+
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -6,19 +6,18 @@ var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var winston    = require('winston');
+var port = process.env.PORT || 80;
+var mongoose = require('mongoose');
+
+mongoose.connect('localhost:27017/api'); // connect to our database
+
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 80;        // set our port
 
-var mongoose = require('mongoose');
-mongoose.connect('localhost:27017/api'); // connect to our database
-
-//Define mongoose Schema
-var Customer = require('../app/models/customer');
 
 // Set up a logger.
 app.locals.logger = new winston.Logger();
@@ -35,112 +34,15 @@ app.use(function (req, res, next) {
 
 app.use(express.static('./app/src'));                 // set the static files location /src/img will be /img for users
 
-
-
-
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
+var customers = require('./api/customers');
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-	res.json({ message: 'How are you? welcome to our api!' });
-});
-
-// on routes that end in /customers
-// ----------------------------------------------------
-router.route('/customers')
-
-// create a Customer (accessed at POST http://localhost:8080/api/customer)
-	.post(function(req, res) {
-
-		var customer = new Customer();
-
-		customer.name = req.body.name;
-		customer.username = req.body.username;
-		customer.password = req.body.password;
-		customer.status = 'NEW';
-		customer.location = req.body.location;
-		customer.meta.age = req.body.age;
-		customer.meta.website = req.body.website;
-		customer.meta.email = req.body.email;
-		customer.meta.phone = req.body.phone;
-		customer.created_at = new Date();
-		customer.updated_at = new Date();
-
-		// save the customer and check for errors
-		customer.save(function(err) {
-			if (err)
-				res.send(err);
-
-			res.json({ message: 'Customer created!' });
-		});
-
-	})
-
-	// get all the customers (accessed at GET http://localhost:8080/api/customer)
-	.get(function(req, res) {
-		Customer.find(function(err, customer) {
-			if (err)
-				res.send(err);
-
-			res.json(customer);
-		});
-	});
-
-// on routes that end in /customers/:customer_id
-// ----------------------------------------------------
-router.route('/customers/:customer_id')
-
-// get the customer with that id (accessed at GET http://localhost:8080/api/customers/:customer_id)
-	.get(function(req, res) {
-		Customer.findById(req.params.customer_id, function(err, customer) {
-			if (err)
-				res.send(err);
-
-			res.json(customer);
-		});
-	})
-	// update
-	.put(function(req, res) {
-		Customer.findById(req.params.customer_id, function(err, customer) {
-
-			if (err)
-				res.send(err);
-
-			customer.name = req.body.name;
-			customer.location = req.body.location;
-			customer.meta.age = req.body.age;
-			customer.meta.website = req.body.website;
-			customer.meta.email = req.body.email;
-			customer.meta.phone = req.body.phone;
-			customer.updated_at = new Date();
-
-			customer.save(function(err) {
-				if (err)
-					res.send(err);
-
-				res.json({ message: 'Customer updated!' });
-			});
-
-		});
-	})
-
-	// delete
-	.delete(function(req, res) {
-		Customer.remove({
-			_id: req.params.customer_id
-		}, function(err, customer) {
-			if (err)
-				res.send(err);
-
-			res.json({ message: 'Successfully deleted' });
-		});
-	});
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api', router);
+app.use('/api', customers);
+
 
 app.get('*', function (req, res) {
 	res.sendfile('./src/index.html'); // load the single view file (angular will handle the page changes on the front-end)
@@ -150,4 +52,4 @@ app.get('*', function (req, res) {
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('How.by fire on port: ' + port);
+console.log('How.by run on port: ' + port);
